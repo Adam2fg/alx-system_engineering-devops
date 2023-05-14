80afbfb2b003d3ce2b9949d2347d460f82961e44
Adam Abah <danielabah45@gmail.com>
2023-05-14 01:50:00 +0100

    Another Commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
-file { '/var/www/html/wp-settings.php':
-  ensure => 'file',
-}
+# Fixes bad `phpp` extensions to `php`
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
-  require => File['/var/www/html/wp-settings.php'],
+  path    => '/usr/local/bin/:/bin/'
 }
