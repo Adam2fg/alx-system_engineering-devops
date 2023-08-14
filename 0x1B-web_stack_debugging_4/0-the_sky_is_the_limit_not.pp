eeeea971be4d6d51678a75261d472d1728248f05
Adam Abah <danielabah45@gmail.com>
2023-08-14 14:13:48 +0100

    Initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Increases the amount of traffic for an Nginx server
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
